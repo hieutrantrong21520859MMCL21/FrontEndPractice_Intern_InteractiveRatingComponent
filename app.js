4310b8fcbf68f3ef4de851eb86f160a2d581b546
DESKTOP-U7NVEFD\LEGION <hieutt03112003@gmail.com>
2024-08-07 19:31:28 +0700

    update event's condition

--- app.js
+++ app.js
@@ -4,11 +4,32 @@ const submit_btn = document.getElementById("submit");
 const rating_buttons = document.querySelectorAll('.rating-box ._button button');
 const result = document.getElementById('result');
 
+// Get number from the clicked button
+let num_star;
+
+rating_buttons.forEach(btn => {
+    
+    btn.addEventListener('click', function() {
+
+        result.innerHTML = btn.innerHTML;
+        num_star = btn.innerHTML;
+    })
+})
+
 // Click on button 'Submit'
 submit_btn.addEventListener('click', function() {
 
-    pop_up.classList.remove('hidden');
-    rating_box.classList.add('hidden');
+    console.log(num_star);
+
+    if (isFinite(num_star))
+    {
+        pop_up.classList.remove('hidden');
+        rating_box.classList.add('hidden');
+    }
+    else
+    {
+        alert('Please rate for us! Thank you!');
+    }
 });
 
 // Click on pop - up to close it
@@ -21,14 +42,6 @@ pop_up.addEventListener('click', function() {
 
         // Reset the rating
         result.innerHTML = "";
+        num_star = undefined;
     } 
 });
-
-// Get number from the clicked button
-rating_buttons.forEach(btn => {
-    
-    btn.addEventListener('click', function() {
-
-        result.innerHTML = btn.innerHTML;
-    })
-})
